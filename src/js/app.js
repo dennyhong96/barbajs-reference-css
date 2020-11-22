c1734dc659b9c72438e5157b64c03e4dee9685fe
Haiyang Hong <hong961127@gmail.com>
2020-11-22 00:05:04 -0800

    Use once hook on page load

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
+import barba from "@barba/core";
+import barbaCss from "@barba/css";
 
+// Use the css plugin
+barba.use(barbaCss);
+
+barba.init({
+  transitions: [
+    {
+      name: "home", // customize css class name space
+      beforeOnce() {
+        console.log("beforeOnce");
+      },
+      // `once` hook happens once on page load
+      once() {
+        console.log("once"); // Will NOT run using CSS plugin, it's used to listen for CSS transition
+      },
+      afterOnce() {
+        console.log("afterOnce");
+      },
+    },
+  ],
+});
